66fd4cc058b9a428ec46c58a049ce3ce6a941c59
PhuongNamHoang198 <hoangphuongnam.98@gmail.com>
2022-10-03 11:44:12 +0700

    congig

---
+++ src/main/java/com/example/role/security/SecurityInit.java
@@ -0,0 +1,6 @@
+package com.example.role.security;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityInit extends AbstractSecurityWebApplicationInitializer {
+}
